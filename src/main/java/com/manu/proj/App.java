2482d20ba9f4fdb4910b3ee6d2af6f3e6dc3fe94
Pavan <pavankalyannaini438@gmail.com>
2021-08-19 21:20:03 +0530

    first commit

---
+++ src/main/java/com/manu/proj/App.java
@@ -0,0 +1,13 @@
+package com.manu.proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
